3a23b49ef285daf7914f2dee5e535d562cceb301
jeroendedauw <jeroendedauw@gmail.com>
2013-11-21 23:10:49 +0100

    Remove broken PHPUnit test runner
    Change-Id: Iaea698882d3ac4dac1610d033b2992369c58a0f4

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo exec( 'composer update' ) . "\n";
-
-require_once( __DIR__ . '/../vendor/autoload.php' );
-
-require_once( __DIR__ . '/../ParamProcessor.php' );
-
-require_once( __DIR__ . '/testLoader.php' );
