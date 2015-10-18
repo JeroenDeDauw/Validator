193495b6e20956382b46e49623be120a6c9eaf41
paladox <paladox@users.noreply.github.com>
2015-10-18 01:23:32 +0100

    Update before_script.sh

--- build/travis/before_script.sh
+++ build/travis/before_script.sh
@@ -16,7 +16,7 @@ mv mediawiki-master phase3/vendor
 
 cd phase3
 
-git checkout $MW
+git checkout master
 
 mysql -e 'create database its_a_mw;'
 php maintenance/install.php --dbtype $DBTYPE --dbuser root --dbname its_a_mw --dbpath $(pwd) --pass nyan TravisWiki admin
